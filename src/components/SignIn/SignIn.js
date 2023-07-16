6f2f5668c55ca0c7e26dda63f596336f1587184d
sathish-goud <sathish.panthangi13@gmail.com>
2023-07-16 15:42:49 +0530

    Adding Form Validations for SigUp and SignIn

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,23 +1,48 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 import '../../styles/SignIn.css';
-import '../../styles/SignUp.css';
+
+const validationSchema = Yup.object({
+  email: Yup.string().email('Invalid email address').required('Email is required'),
+  password: Yup.string().required('Password is required'),
+});
+
 const SignIn = () => {
+  const initialValues = {
+    email: '',
+    password: '',
+  };
+
+  const handleSubmit = (values) => {
+    // Handle form submission logic here
+    console.log(values);
+  };
+
   return (
     <div className="container">
       <h1>Sign In</h1>
-      <form>
-        <div className="form-group">
-          <label htmlFor="email">Email address</label>
-          <input type="email" className="form-control" id="email" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input type="password" className="form-control" id="password" />
-        </div>
-        <button type="submit" className="btn btn-primary">Sign In</button>
-      </form>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        <Form>
+          <div className="form-group">
+            <label htmlFor="email">Email address</label>
+            <Field type="email" id="email" name="email" className="form-control" />
+            <ErrorMessage name="email" component="div" className="error-message" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <Field type="password" id="password" name="password" className="form-control" />
+            <ErrorMessage name="password" component="div" className="error-message" />
+          </div>
+          <button type="submit" className="btn btn-primary">Sign In</button>
+        </Form>
+      </Formik>
     </div>
   );
-}
+};
 
 export default SignIn;
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,26 +1,57 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 import '../../styles/SignUp.css';
+
+const validationSchema = Yup.object({
+  name: Yup.string().required('Name is required'),
+  email: Yup.string().email('Invalid email address').required('Email is required'),
+  password: Yup.string()
+    .min(8, 'Password must be at least 8 characters')
+    .required('Password is required'),
+});
+
 const SignUp = () => {
+  const initialValues = {
+    name: '',
+    email: '',
+    password: '',
+  };
+
+  const handleSubmit = (values) => {
+    // Handle form submission logic here
+    console.log(values);
+  };
+
   return (
     <div className="container">
       <h1>Sign Up</h1>
-      <form>
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input type="text" className="form-control" id="name" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email address</label>
-          <input type="email" className="form-control" id="email" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input type="password" className="form-control" id="password" />
-        </div>
-        <button type="submit" className="btn btn-primary">Sign Up</button>
-      </form>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        <Form>
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <Field type="text" id="name" name="name" className="form-control" />
+            <ErrorMessage name="name" component="div" className="error-message" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email address</label>
+            <Field type="email" id="email" name="email" className="form-control" />
+            <ErrorMessage name="email" component="div" className="error-message" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <Field type="password" id="password" name="password" className="form-control" />
+            <ErrorMessage name="password" component="div" className="error-message" />
+          </div>
+          <button type="submit" className="btn btn-primary">Sign Up</button>
+        </Form>
+      </Formik>
     </div>
   );
-}
+};
 
 export default SignUp;
